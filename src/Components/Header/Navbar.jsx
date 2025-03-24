3bfb38123b0287f17a43f184f15221c9a2fa2171
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-25 03:26:28 +0600

    complete navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,14 +1,15 @@
 import './Navbar.css';
 const Navbar = () => {
+
     return (
-        <div className='w-11/12 mx-auto p-5 s1'>
+        <div className='w-11/12 mx-auto s1 p-6 '>
             <img src="logo.png" alt="" />
-            <ul className='s1 pr-2'>
+            <ul className='s1 text-xl'>
                 <li><a href="home.jsx">Home</a></li>
                 <li><a href="about.jsx">About</a></li>
                 <li><a href="Teams.jsx">Teams</a></li>
                 <li><a href="Schedules.jsx">Schedules</a></li>
-                <li className='flex font-bold gap-2 w-6'> Coin  <img src="coins.png" alt="" /></li>
+                <li className='flex font-bold gap-2 w-6 ml-8'> Coins  <img src="coins.png" alt="" /></li>
             </ul>
         </div>
     );
--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,8 +1,17 @@
 import './HeroSection.css';
 const HeroSection = () => {
-    return (
-        <div>
 
+    return (
+        <div className='w-11/12 mx-auto'>
+
+            <div className=' bgg h1'>
+                <img src="banner-main.png" alt="" />
+                <div className='text-5xl font-semibold text-white pt-5'>
+                    <h2>Assemble Your Ultimate Dream 11 Cricket Team</h2>
+                </div>
+                <p className='text-gray-300 text-3xl p-5'>Beyond Boundaries Beyond Limits</p>
+                <button className='text-white'>Claim Free Credit</button>
+            </div>
         </div>
     );
 };
